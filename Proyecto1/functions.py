b76b774ae7c1fe108dea2dea48ba1d258ec0dc28
xavieroldan <xavieroldan@gmail.com>
2019-05-31 09:54:57 +0200

    Commit from ide

---
+++ Proyecto1/functions.py
@@ -0,0 +1,11 @@
+#definir una función
+# se hacen antes del main
+
+def sayHello ():
+    name = input("Duál es tu nombre?")
+    print("Hola "+name)
+
+if __name__ == '__main__':
+
+    sayHello()
+
