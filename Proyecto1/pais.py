55606fb94b5a86b0df69bd05385ecae05619104b
xavieroldan <xavieroldan@gmail.com>
2019-05-31 11:14:58 +0200

    Return values in a function

--- Proyecto1/pais.py
+++ Proyecto1/pais.py
@@ -1,13 +1,23 @@
 # me pida el nombre de un pais
 # me imprimer le nombre de un pais
 
-def setCountry():
-    return input("Dame el país")
+# def setCountry():
+#     return input("Dame el país")
+#
+# def printCountry(input):
+#     print("El país es "+ input)
 
-def printCountry(input):
-    print("El país es "+ input)
+#funcion suma: dado 2 números nos devuelve la suma de ambos
+
+def suma(num1, num2):
+    output = num1 + num2
+    return output
+
+
 
 if __name__ == '__main__':
+        #
+        # country = setCountry()
+        # printCountry(country)
 
-        country = setCountry()
-        printCountry(country)
+#funcion suma: dado 2 números nos devuelve la suma de ambos
